b5af55c291aafff28711b2a8ecb657842790fd38
rwe <rwe@tiqri.com>
2018-01-29 19:53:01 +0530

    added pagecount and /

--- server.js
+++ server.js
@@ -56,10 +56,42 @@ var initDb = function(callback) {
   });
 };
 
-
 app.get('/', function (req, res) {
-    res.status(200).json({ "message": "Welcome to Lyrics application." });
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      if (err) {
+        console.log('Error running count. Message:\n'+err);
+      }
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
 });
+
+app.get('/pagecount', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    db.collection('counts').count(function(err, count ){
+      res.send('{ pageCount: ' + count + '}');
+    });
+  } else {
+    res.send('{ pageCount: -1 }');
+  }
+});
+
 app.get('/collections', function (req, res) {
     if (!db) {
         initDb(function (err) { });
@@ -264,38 +296,3 @@ module.exports = app ;
 
 
 
-// app.get('/', function (req, res) {
-//   // try to initialize the db on every request if it's not already
-//   // initialized.
-//   if (!db) {
-//     initDb(function(err){});
-//   }
-//   if (db) {
-//     var col = db.collection('counts');
-//     // Create a document with request IP and current time of request
-//     col.insert({ip: req.ip, date: Date.now()});
-//     col.count(function(err, count){
-//       if (err) {
-//         console.log('Error running count. Message:\n'+err);
-//       }
-//       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-//     });
-//   } else {
-//     res.render('index.html', { pageCountMessage : null});
-//   }
-// });
-
-// app.get('/pagecount', function (req, res) {
-//   // try to initialize the db on every request if it's not already
-//   // initialized.
-//   if (!db) {
-//     initDb(function(err){});
-//   }
-//   if (db) {
-//     db.collection('counts').count(function(err, count ){
-//       res.send('{ pageCount: ' + count + '}');
-//     });
-//   } else {
-//     res.send('{ pageCount: -1 }');
-//   }
-// });
